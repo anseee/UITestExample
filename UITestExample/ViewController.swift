eb82025fa842dc635e75bc3930bcbdb8b8ecb45d
ansee <admin@anse.kr>
2017-02-13 13:18:06 +0900

    project.pbxproj, Info.plist, ViewController.swift, OHHTTPStubsSwift.swift
    add ohhttpstups’s helper class
    add example source code but not running

--- UITestExample/ViewController.swift
+++ UITestExample/ViewController.swift
@@ -7,12 +7,24 @@
 //
 
 import UIKit
+import OHHTTPStubs
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+
+        // Swift
+        stub(condition: isHost("http://127.0.0.1:5000/todo/api/v1.0/tasks")) { request in
+            // Stub it with our "wsresponse.json" stub file
+            return OHHTTPStubsResponse(
+                fileAtPath: OHPathForFile("wsresponse.json", type(of: self))!,
+                statusCode: 200,
+                headers: ["Content-Type":"application/json"]
+            )
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
